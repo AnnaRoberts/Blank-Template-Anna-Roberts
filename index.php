c4964383c296e08920c98d4d8d65eb22ca3a3424
Anna Roberts <annaroberts960608@gmail.com>
2016-02-11 11:10:57 -0500

    will add simple first steps to Them Dev

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Web Interactivity and Engagement</title>
+</head>
+<body>
+	<h1>Web Interactivity and Engagement</h1>
+	<h2>Spring 2016</h2>
+
+	<p> This is where the blog will go. </p>
+
+</body>
+</html>
