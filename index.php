90054d394ae42a67df2e8427d52c4dff9c05fc21
Anna Roberts <annaroberts960608@gmail.com>
2016-02-11 11:43:05 -0500

    add skeleton/ normalize css files and add css links

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<title> Web Interactivity and Engagement</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<h1>Web Interactivity and Engagement</h1>
 	<h2>Spring 2016</h2>
 
 	<p> This is where the blog will go. </p>
+
 
 </body>
 </html>
