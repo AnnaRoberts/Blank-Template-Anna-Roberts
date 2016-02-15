db23dbea8ab51ea67e6bde7c175566ee136aa4b2
Anna Roberts <annaroberts960608@gmail.com>
2016-02-15 10:33:09 -0500

    add wigets for menu and footer

--- function.php
+++ function.php
@@ -8,9 +8,43 @@ function blank_widgets_init() {
         'description' => 'Widget for our sidebar on pages', 
         'before_widget' => '<div class="widget-sidebar">', 
         'after_widget' => '</div>',
-        'before_title' => '<h2>',
-        'after_title' => '</h2>'                        
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
         ));
-    }
+    register_sidebar( array(
+        'name' => ('Footer Left'),
+        'id' => 'footer-left',
+        'description' => 'Widget for the left side of our footer', 
+        'before_widget' => '<div class="widget-sidebar widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+    register_sidebar( array(
+        'name' => ('Footer Middle'),
+        'id' => 'footer-middle',
+        'description' => 'Widget for the middle of our footer', 
+        'before_widget' => '<div class="widget-sidebar widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+    register_sidebar( array(
+        'name' => ('Footer Right'),
+        'id' => 'footer-right',
+        'description' => 'Widget for the right side of our footer', 
+        'before_widget' => '<div class="widget-sidebar widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+}
 add_action('widgets_init', 'blank_widgets_init');
+
+/*-------------- Enable Menu --------------- */
+
+add_theme_support('menus');
+
+
+
 ?>
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title> Web Interactivity and Engagement</title>
+	<title><?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 <?php wp_head(); ?>
 </head>
@@ -10,9 +10,16 @@
 	<div class ="container">
 		<header class="row">
 			<div class="twelve columns">
-				<h1><a href="<?php bloginfo('url'); ?>"<?php bloginfo('name'); ?></a></h1>
+				<h1><a href="<?php bloginfo('url'); ?>"> <?php bloginfo('name');?> </a> </h1>
 				<h2> <?php bloginfo('description'); ?></h2>
 			</div>
 		</header>
-<!-- END HEADER -->
-
+		<!-- Add Menu Here -->
+		<div class="row">
+		    <div class="twelve columns">
+		        <?php wp_nav_menu(array(
+		            'sort_column' => 'menu_order', 
+		            'container_class' => 'blank-menu-header'
+		            ));?>
+		    </div>
+		</div>
